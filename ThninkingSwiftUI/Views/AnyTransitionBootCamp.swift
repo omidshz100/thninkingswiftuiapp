d10af4b43c0c752361a1501c1b4a660b8329d5ab
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-05-15 13:21:42 +0200

    until here I added two things to the AnyTransition with Extension, one variable with transition, another Function that produce transition rotation based on angel we tell
    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    Persian :
    در این مرحله من transition رو اضافه کردم بصورت تابع و متغییر و  انیمیشن چرخشی برای نمایش و عدم نمایش ویو به وجود اومد
    ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- ThninkingSwiftUI/Views/AnyTransitionBootCamp.swift
+++ ThninkingSwiftUI/Views/AnyTransitionBootCamp.swift
@@ -12,12 +12,18 @@ struct RotateViewModifier:ViewModifier{
     func body(content: Content) -> some View {
         content
             .rotationEffect(Angle(degrees: rotation))
+            .offset(x: rotation != 0 ? UIScreen.main.bounds.width : 0,
+                    y: rotation != 0 ? UIScreen.main.bounds.height : 0)
     }
 }
 
 extension AnyTransition{
     static var rotating:AnyTransition {
         return AnyTransition.modifier(active: RotateViewModifier(rotation: 180), identity: RotateViewModifier(rotation: 0))
+    }
+    
+    static func rotating(rotation:Double)->AnyTransition{
+        return AnyTransition.modifier(active: RotateViewModifier(rotation: rotation), identity: RotateViewModifier(rotation: 0))
     }
 
 }
@@ -30,14 +36,15 @@ struct AnyTransitionBootCamp: View {
                 RoundedRectangle(cornerRadius: 25)
                     .frame(width: 250, height: 350)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .transition(AnyTransition.rotating.animation(.easeOut))
+                    //.transition(AnyTransition.rotating.animation(.easeOut))
+                    .transition(.rotating(rotation: 1080))
             }
             Spacer()
             Text("Click Me!")
                 .withDefualtButtonViewFormatter()
                 .padding(.horizontal, 40)
                 .onTapGesture {
-                    withAnimation(.easeOut) {
+                    withAnimation(.easeInOut(duration: 5.0)) {
                         showRectangle.toggle()
                     }
                 }
