608043f9bd0b60952b6c05bc5eba16dd5c714079
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-05-15 10:37:55 +0200

    Adding function to the view to have custom button style

--- ThninkingSwiftUI/Views/ButtonStyleBootcamp.swift
+++ ThninkingSwiftUI/Views/ButtonStyleBootcamp.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ButtonStylableStyle:ButtonStyle{
+    let scaledAmount: CGFloat
+    init(scaledAmount: CGFloat = 0.9) {
+        self.scaledAmount = scaledAmount
+    }
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .brightness(configuration.isPressed ? 0.5:1.0)
-            //.opacity(configuration.isPressed ? 0.8:1.0)
+            .scaleEffect(configuration.isPressed ? scaledAmount:1.0)
+            //.brightness(configuration.isPressed ? 0.2:0.0)
+            .opacity(configuration.isPressed ? 0.8:1.0)
     }
 }
 
+extension View {
+    func withPressableStyle(scaleAmount:CGFloat = 0.9)-> some View {
+        // here I removed the self, because Xcode understand buttonStyle is a modifier added to the view to generate another
+        buttonStyle(ButtonStylableStyle(scaledAmount: scaleAmount))
+    }
+}
 struct ButtonStyleBootcamp: View {
     var body: some View {
         Button {
@@ -29,7 +40,8 @@ struct ButtonStyleBootcamp: View {
                 .cornerRadius(10)
                 .shadow(color: Color.blue.opacity(0.3), radius: 10, x: 0.0, y: 10)
         }
-        .buttonStyle(ButtonStylableStyle())
+        //.buttonStyle(ButtonStylableStyle(scaledAmount: 0.5))
+        .withPressableStyle(scaleAmount: 0.5)
         .padding(40)
 
     }
